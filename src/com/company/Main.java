32a6814aa96491d42dd9a06821218ab1f596eb21
jacobwhiteman17 <j1whiteman@student.bridgew.edu>
2019-10-01 21:11:57 -0400

    Added University class, started work on Peofessor class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        var myUniversity = new University();
+        myUniversity.runUniversity();
     }
 }
--- src/com/company/Professor.java
+++ src/com/company/Professor.java
@@ -1,4 +1,15 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Professor {
+    private ArrayList<Student> adviseeList;
+    private String name;
+    private String department;
+
+    Professor(String profName, String dept){
+        name = profName;
+        department = dept;
+        adviseeList = new ArrayList<Student>();
+    }
 }
