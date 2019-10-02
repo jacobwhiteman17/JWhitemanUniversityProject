39e89274a7edc89280088cf38d19726c665eea43
jacobwhiteman17 <j1whiteman@student.bridgew.edu>
2019-10-02 12:51:16 -0400

    Professor class is almost done. Think I implemented array list wrong but I'll fix that later

--- src/com/company/Professor.java
+++ src/com/company/Professor.java
@@ -12,4 +12,23 @@ public class Professor {
         department = dept;
         adviseeList = new ArrayList<Student>();
     }
+
+    public void addAdvisee(Student newAdvisee){
+        var student = newAdvisee;
+        adviseeList.add(student);
+    }
+
+    public void removeAdvisee(Student formerAdvisee){
+        var student = formerAdvisee;
+        adviseeList.remove(student);
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String toString(){
+        String profInfo = "Professor: "+name+" Department: "+department;
+        return profInfo;
+    }
 }
